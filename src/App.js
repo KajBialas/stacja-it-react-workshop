ddfc2333c324cbb2f5367f93ab535c8fe34a1b99
Kaj Białas <kaj.bialas@dev-industry.com>
2020-04-26 10:40:53 +0200

    Issue2 final

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       Przykładowy napis
-    </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <header>Stopka strony</header>
+  )
+}
+
+export default Footer;
