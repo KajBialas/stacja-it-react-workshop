18633581d0593e458e2e9b6eb627db1afeed2e0f
Kaj Białas <kaj.bialas@dev-industry.com>
2020-04-26 10:46:18 +0200

    Issue 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import TextSection from './TextSection';
 
 function App() {
   return (
     <>
       <Header />
+      <TextSection />
       Przykładowy napis
       <Footer />
     </>
---
+++ src/TextSection.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TextSection extends Component {
+  render() {
+    return (
+      <div>
+        Sekcja tekstowa
+      </div>
+    )
+  }
+}
+
+export default TextSection;
