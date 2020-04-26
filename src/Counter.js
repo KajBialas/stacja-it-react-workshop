ea1f005bb49041f515424c57203122d665e0fc02
Kaj Białas <kaj.bialas@dev-industry.com>
2020-04-26 12:59:15 +0200

    Basic version

--- src/Counter.js
+++ src/Counter.js
@@ -11,12 +11,26 @@ class Counter extends Component {
     });
   };
 
+  decrementCounter = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
+  resetCounter = () => {
+    this.setState({
+      count: 0,
+    });
+  };
+
   render() {
     return (
       <div>
         Counter: {this.state.count}
         <div>
           <button onClick={this.incrementCounter}>+</button>
+          <button onClick={this.decrementCounter}>-</button>
+          <button onClick={this.resetCounter}>RESET</button>
         </div>
       </div>
     )
